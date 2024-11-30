00e369ad9b4ede6b97b0337966b4f5c35bca9e55
adrian-trichet <adrian.trichet@laplateforme.io>
2024-11-30 19:43:35 +0100

    Affichage de la grille en console

---
+++ main.py
@@ -0,0 +1,21 @@
+def init_grid():
+    # Retourne une liste de 3 sous-listes, chacune contenant 3 espaces vides
+    # Cela représente une grille 3x3 initialisée pour le jeu
+    # "_" est une convention qui permet de ne pas utiliser une variable dans une boucle for
+    # dans ce cas elle permet juste de répéter l'action 3 fois
+    return [[' ' for _ in range(3)] for _ in range(3)]
+
+def display_grid(grid):
+    # Parcourt chaque ligne de la grille
+    for row in grid:
+        # Affiche la ligne actuelle en joignant les cases avec une barre verticale (|)
+        print('|'.join(row))
+        
+        # Affiche une ligne de séparation entre les lignes de la grille
+        # Cette ligne de séparation a une longueur de 5 (trois colonnes et deux séparateurs)
+        print('-' * 5)
+
+
+grid = init_grid()
+
+display_grid(grid)
